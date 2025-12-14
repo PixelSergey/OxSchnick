7bf249764e83d7e0d6f6bd989db20f89beaefa13
Helena Jäger <mail@helena.place>
2025-12-14 22:03:20 +0100

    feat: dect + root user in migration

--- migrations/2025-11-18-193403-0000_create_users/up.sql
+++ migrations/2025-11-18-193403-0000_create_users/up.sql
@@ -1,8 +1,11 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username varchar(32),
+    dect char(4),
     parent integer references users(id) NOT NULL,
     token char(36) NOT NULL,
     created timestamptz NOT NULL,
     active boolean NOT NULL
-)
+);
+
+INSERT INTO users (username, parent, token, created, active) VALUES ('root', lastval(), uuidv4(), now(), true);
