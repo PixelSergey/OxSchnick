67c1403517371d33ae75db4c5403bba7e4fb0bb9
Meisenburger13 <melissa.eisenburger@gmail.com>
2025-12-18 20:57:01 +0100

    feat: changed icon colors to fit our theme

--- src/main.rs
+++ src/main.rs
@@ -117,23 +117,24 @@ async fn main() {
                 serve_static!(
                     &file[..],
                     [
-                        ["style.css", "../assets/style.css", "text/css"],
-                        ["phone_receiver.svg", "../assets/phone_receiver.svg", "image/svg+xml"],
-                        ["rock.svg", "../assets/rock.svg", "image/svg+xml"],
-                        ["paper.svg", "../assets/paper.svg", "image/svg+xml"],
-                        ["scissors.svg", "../assets/scissors.svg", "image/svg+xml"],
-                        ["won.svg", "../assets/won.svg", "image/svg+xml"],
-                        ["lost.svg", "../assets/lost.svg", "image/svg+xml"],
                         ["abort.svg", "../assets/abort.svg", "image/svg+xml"],
                         ["adult.svg", "../assets/adult.svg", "image/svg+xml"],
-                        ["hash_char.svg", "../assets/hash_char.svg", "image/svg+xml"],
-                        ["spider_web.svg", "../assets/spider_web.svg", "image/svg+xml"],
+                        ["arrow_back.svg", "../assets/arrow_back.svg", "image/svg+xml"],
+                        ["arrow_right.svg", "../assets/arrow_right.svg", "image/svg+xml"],
                         ["children.svg", "../assets/children.svg", "image/svg+xml"],
                         ["distance.svg", "../assets/distance.svg", "image/svg+xml"],
+                        ["hash_char.svg", "../assets/hash_char.svg", "image/svg+xml"],
+                        ["lost.svg", "../assets/lost.svg", "image/svg+xml"],
+                        ["paper.svg", "../assets/paper.svg", "image/svg+xml"],
+                        ["phone_receiver.svg", "../assets/phone_receiver.svg", "image/svg+xml"],
+                        ["rock.svg", "../assets/rock.svg", "image/svg+xml"],
+                        ["scissors.svg", "../assets/scissors.svg", "image/svg+xml"],
                         ["score.svg", "../assets/score.svg", "image/svg+xml"],
+                        ["spider_web.svg", "../assets/spider_web.svg", "image/svg+xml"],
                         ["streak.svg", "../assets/streak.svg", "image/svg+xml"],
-                        ["wrench.svg", "../assets/wrench.svg", "image/svg+xml"],
-                        ["arrow_right.svg", "../assets/arrow_right.svg", "image/svg+xml"]
+                        ["style.css", "../assets/style.css", "text/css"],
+                        ["won.svg", "../assets/won.svg", "image/svg+xml"],
+                        ["wrench.svg", "../assets/wrench.svg", "image/svg+xml"]
                     ]
                 )
             }),
