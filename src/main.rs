ff430458d54b800a7f1e95729fbcea9a273c105f
Meisenburger13 <melissa.eisenburger@gmail.com>
2025-12-19 14:12:42 +0100

    refactor: add subfolders for icons and make names clearer
    when placeholder icons are replaced, only the svg will need to be updated

--- src/main.rs
+++ src/main.rs
@@ -117,24 +117,29 @@ async fn main() {
                 serve_static!(
                     &file[..],
                     [
-                        ["abort.svg", "../assets/abort.svg", "image/svg+xml"],
-                        ["adult.svg", "../assets/adult.svg", "image/svg+xml"],
+                        // metrics
+                        ["distance.svg", "../assets/metrics/distance.svg", "image/svg+xml"],
+                        ["num_invites.svg", "../assets/metrics/num_invites.svg", "image/svg+xml"],
+                        ["num_schnicks.svg", "../assets/metrics/num_schnicks.svg", "image/svg+xml"],
+                        ["score.svg", "../assets/metrics/score.svg", "image/svg+xml"],
+                        ["streak.svg", "../assets/metrics/streak.svg", "image/svg+xml"],
+                        // navigation bar
+                        ["graphs.svg", "../assets/nav_bar/graphs.svg", "image/svg+xml"],
+                        ["home.svg", "../assets/nav_bar/home.svg", "image/svg+xml"],
+                        ["metrics.svg", "../assets/nav_bar/metrics.svg", "image/svg+xml"],
+                        ["settings.svg", "../assets/nav_bar/settings.svg", "image/svg+xml"],
+                        // schnick
+                        ["abort.svg", "../assets/schnick/abort.svg", "image/svg+xml"],
+                        ["lost.svg", "../assets/schnick/lost.svg", "image/svg+xml"],
+                        ["paper.svg", "../assets/schnick/paper.svg", "image/svg+xml"],
+                        ["rock.svg", "../assets/schnick/rock.svg", "image/svg+xml"],
+                        ["scissors.svg", "../assets/schnick/scissors.svg", "image/svg+xml"],
+                        ["won.svg", "../assets/schnick/won.svg", "image/svg+xml"],
+                        // utils
                         ["arrow_back.svg", "../assets/arrow_back.svg", "image/svg+xml"],
                         ["arrow_right.svg", "../assets/arrow_right.svg", "image/svg+xml"],
-                        ["children.svg", "../assets/children.svg", "image/svg+xml"],
-                        ["distance.svg", "../assets/distance.svg", "image/svg+xml"],
-                        ["hash_char.svg", "../assets/hash_char.svg", "image/svg+xml"],
-                        ["lost.svg", "../assets/lost.svg", "image/svg+xml"],
-                        ["paper.svg", "../assets/paper.svg", "image/svg+xml"],
-                        ["phone_receiver.svg", "../assets/phone_receiver.svg", "image/svg+xml"],
-                        ["rock.svg", "../assets/rock.svg", "image/svg+xml"],
-                        ["scissors.svg", "../assets/scissors.svg", "image/svg+xml"],
-                        ["score.svg", "../assets/score.svg", "image/svg+xml"],
-                        ["spider_web.svg", "../assets/spider_web.svg", "image/svg+xml"],
-                        ["streak.svg", "../assets/streak.svg", "image/svg+xml"],
-                        ["style.css", "../assets/style.css", "text/css"],
-                        ["won.svg", "../assets/won.svg", "image/svg+xml"],
-                        ["wrench.svg", "../assets/wrench.svg", "image/svg+xml"]
+                        ["phone.svg", "../assets/phone.svg", "image/svg+xml"],
+                        ["style.css", "../assets/style.css", "text/css"]
                     ]
                 )
             }),
