449800d173c8e8d00c4923cdb81fdfdd9424e1e6
Helena Jäger <mail@helena.place>
2025-12-20 12:15:11 +0100

    fix: asset naming after merge

--- src/routes/assets.rs
+++ src/routes/assets.rs
@@ -21,32 +21,37 @@ pub async fn assets(Path(file): Path<String>) -> Result<impl IntoResponse, Statu
         [
             ["style.css", "../../assets/style.css", "text/css"],
             [
-                "phone_receiver.svg",
-                "../../assets/phone_receiver.svg",
+                "phone.svg",
+                "../../assets/phone.svg",
                 "image/svg+xml"
             ],
-            ["rock.svg", "../../assets/rock.svg", "image/svg+xml"],
-            ["paper.svg", "../../assets/paper.svg", "image/svg+xml"],
-            ["scissors.svg", "../../assets/scissors.svg", "image/svg+xml"],
-            ["won.svg", "../../assets/won.svg", "image/svg+xml"],
-            ["lost.svg", "../../assets/lost.svg", "image/svg+xml"],
-            ["abort.svg", "../../assets/abort.svg", "image/svg+xml"],
-            ["adult.svg", "../../assets/adult.svg", "image/svg+xml"],
+            ["rock.svg", "../../assets/schnick/rock.svg", "image/svg+xml"],
+            ["paper.svg", "../../assets/schnick/paper.svg", "image/svg+xml"],
+            ["scissors.svg", "../../assets/schnick/scissors.svg", "image/svg+xml"],
+            ["won.svg", "../../assets/schnick/won.svg", "image/svg+xml"],
+            ["lost.svg", "../../assets/schnick/lost.svg", "image/svg+xml"],
+            ["abort.svg", "../../assets/schnick/abort.svg", "image/svg+xml"],
+            ["home.svg", "../../assets/nav_bar/home.svg", "image/svg+xml"],
             [
-                "hash_char.svg",
-                "../../assets/hash_char.svg",
+                "metrics.svg",
+                "../../assets/nav_bar/metrics.svg",
                 "image/svg+xml"
             ],
             [
-                "spider_web.svg",
-                "../../assets/spider_web.svg",
+                "graphs.svg",
+                "../../assets/nav_bar/graphs.svg",
                 "image/svg+xml"
             ],
-            ["children.svg", "../../assets/children.svg", "image/svg+xml"],
-            ["distance.svg", "../../assets/distance.svg", "image/svg+xml"],
-            ["score.svg", "../../assets/score.svg", "image/svg+xml"],
-            ["streak.svg", "../../assets/streak.svg", "image/svg+xml"],
-            ["wrench.svg", "../../assets/wrench.svg", "image/svg+xml"],
+            ["num_invites.svg", "../../assets/metrics/num_invites.svg", "image/svg+xml"],
+            ["distance.svg", "../../assets/metrics/distance.svg", "image/svg+xml"],
+            ["score.svg", "../../assets/metrics/score.svg", "image/svg+xml"],
+            ["streak.svg", "../../assets/metrics/streak.svg", "image/svg+xml"],
+            ["settings.svg", "../../assets/nav_bar/settings.svg", "image/svg+xml"],
+            [
+                "arrow_back.svg",
+                "../../assets/arrow_back.svg",
+                "image/svg+xml"
+            ],
             [
                 "arrow_right.svg",
                 "../../assets/arrow_right.svg",
