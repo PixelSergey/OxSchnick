4a946373c90e99c5f437c5e7d149f44ed170bebb
Helena Jäger <mail@helena.place>
2025-12-21 02:14:28 +0100

    feat: fix nixos module

--- flake.nix
+++ flake.nix
@@ -38,65 +38,66 @@
           drv = packages.fanschnick-server; 
         };
       };
-      nixosModules = rec {
-        default = fanschnick-server;
-        fanschnick-server = { config, lib, ... }:
-          let cfg = config.services.fanschnick-server; in {
-            options = with lib.types; {
-            services.fanschnick-server = {
-              enable = lib.mkEnableOption "Enable Fanschnick";
-              host = lib.mkOption { type = str; description = "Virtual Host to serve under"; };
-              port = lib.mkOption { type = port; default = 8080; description = "Local port to bind to"; };
-            };
-            };
-            config.services.postgresql = lib.mkIf cfg.enable {
-              enable = true;
-              package = pkgs.postgresql_18;
-              ensureDatabases = [ "fanschnick" ];
-              ensureUsers = [{
-                name = "fanschnick";
-                ensureDBOwnership = true;
-                ensureClauses.login = true;
-                ensureClauses.createdb = true;
-              }];
-              authentication = pkgs.lib.mkOverride 10 ''
-                #type database  DBuser  auth-method
-                local all       all     trust
-                host  all       all     127.0.0.1/32 trust
-                host  all       all     ::1/128      trust
-              '';
-            };
-            config.services.nginx.virtualHosts = lib.mkIf cfg.enable {
-              ${cfg.host} = {
-                forceSSL = true;
-                enableACME = true;
-                locations."/" = {
-                  proxyPass = "http://127.0.0.1:${cfg.port}"; 
-                  recommendedProxySettings = true;
-                };
-                locations."/.well-known/".root = "/var/lib/acme/acme-challenge";
-              };
-            };
-            config.users.groups.fanschnick = {};
-            config.users.users.fanschnick = lib.mkIf cfg.enable {
-              isNormalUser = true;
-              group = "fanschnick";
-            };
-            config.systemd.services.fanschnick-server = lib.mkIf cfg.enable {
-              description = "Fanschnick Server";
-              wantedBy = [ "multi-user.target" ];
-              after = [ "network.target" "postgresql.target" ];
-              environment = {
-                "DATABASE_URL" = "postgres://localhost/fanschnick";
-              };
-              serviceConfig = {
-                User = "fanschnick";
-                Group = "fanschnick";
-                ExecStart = "${packages.fanschnick-server}/bin/fanschnick-server https://${cfg.host} 127.0.0.1:${cfg.port}";
-              };
-            };
-          };
-      };
     }
   );
+  
+  nixosModules = rec {
+    default = fanschnick-server;
+    fanschnick-server = { config, lib, ... }:
+      let cfg = config.services.fanschnick-server; in {
+        options = with lib.types; {
+        services.fanschnick-server = {
+          enable = lib.mkEnableOption "Enable Fanschnick";
+          host = lib.mkOption { type = str; description = "Virtual Host to serve under"; };
+          port = lib.mkOption { type = port; default = 8080; description = "Local port to bind to"; };
+        };
+        };
+        config.services.postgresql = lib.mkIf cfg.enable {
+          enable = true;
+          package = pkgs.postgresql_18;
+          ensureDatabases = [ "fanschnick" ];
+          ensureUsers = [{
+            name = "fanschnick";
+            ensureDBOwnership = true;
+            ensureClauses.login = true;
+            ensureClauses.createdb = true;
+          }];
+          authentication = pkgs.lib.mkOverride 10 ''
+            #type database  DBuser  auth-method
+            local all       all     trust
+            host  all       all     127.0.0.1/32 trust
+            host  all       all     ::1/128      trust
+          '';
+        };
+        config.services.nginx.virtualHosts = lib.mkIf cfg.enable {
+          ${cfg.host} = {
+            forceSSL = true;
+            enableACME = true;
+            locations."/" = {
+              proxyPass = "http://127.0.0.1:${cfg.port}"; 
+              recommendedProxySettings = true;
+            };
+            locations."/.well-known/".root = "/var/lib/acme/acme-challenge";
+          };
+        };
+        config.users.groups.fanschnick = {};
+        config.users.users.fanschnick = lib.mkIf cfg.enable {
+          isNormalUser = true;
+          group = "fanschnick";
+        };
+        config.systemd.services.fanschnick-server = lib.mkIf cfg.enable {
+          description = "Fanschnick Server";
+          wantedBy = [ "multi-user.target" ];
+          after = [ "network.target" "postgresql.target" ];
+          environment = {
+            "DATABASE_URL" = "postgres://localhost/fanschnick";
+          };
+          serviceConfig = {
+            User = "fanschnick";
+            Group = "fanschnick";
+            ExecStart = "${self.packages.x86_64.fanschnick-server}/bin/fanschnick-server https://${cfg.host} 127.0.0.1:${cfg.port}";
+          };
+        };
+      };
+  };
 }
