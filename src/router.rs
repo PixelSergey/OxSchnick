3f7e1c142cef36fa516fb373b1cf84568f4574ae
Helena Jäger <mail@helena.place>
2025-12-25 14:16:21 +0100

    feat: add redirection if in schnick

--- src/router.rs
+++ src/router.rs
@@ -1,19 +1,30 @@
 use std::sync::Arc;
 
 use axum::{
-    Router,
-    middleware::from_fn_with_state,
-    routing::{get, post},
+    Router, extract::{self, Request}, middleware::{Next, from_fn_with_state}, response::{IntoResponse, Redirect}, routing::{get, post}
 };
 use diesel_async::{AsyncPgConnection, pooled_connection::bb8::Pool};
 use tokio::sync::RwLock;
 use url::Url;
 
 use crate::{
-    auth::Authenticator, error::Error, graphs::Graph, metrics::Metrics, routes::{
-        about, assets, graphs, graphs_cache, graphs_global, graphs_graph, graphs_sse, graphs_tree, home, home_invite, home_sse, imprint, index, invite, invite_accept, metrics, metrics_score, metrics_num_schnicks, metrics_streak, metrics_num_invites, schnick, schnick_abort, schnick_sse, schnick_submit, settings, settings_dect, settings_username
+    auth::{Authenticator, User}, error::Error, graphs::Graph, metrics::Metrics, routes::{
+        about, assets, graphs, graphs_cache, graphs_global, graphs_graph, graphs_sse, graphs_tree, home, home_invite, home_sse, imprint, index, invite, invite_accept, metrics, metrics_num_invites, metrics_num_schnicks, metrics_score, metrics_streak, schnick, schnick_abort, schnick_sse, schnick_submit, settings, settings_dect, settings_username
     }, schnicks::Schnicker, state::State
 };
+
+pub async fn redirect_if_in_schnick(
+    extract::State(state): extract::State<State>,
+    User(id): User,
+    request: Request,
+    next: Next
+) -> impl IntoResponse {
+    if let Ok(_) = Schnicker::request_in_schnick(id, &state.schnicker).await {
+        Redirect::to("/schnick").into_response()
+    } else {
+        next.run(request).await
+    }
+}
 
 pub async fn router(
     base_url: Url,
@@ -56,15 +67,7 @@ pub async fn router(
         .with_state(state.clone());
     let authenticated = Router::new()
         .route("/home", get(home))
-        .route("/home/sse", get(home_sse))
-        .route("/home/invite", get(home_invite))
-        .route("/schnick", get(schnick))
-        .route("/schnick", post(schnick_submit))
-        .route("/schnick/sse", get(schnick_sse))
-        .route("/schnick/abort", get(schnick_abort))
         .route("/settings", get(settings))
-        .route("/settings/dect", post(settings_dect))
-        .route("/settings/username", post(settings_username))
         .route("/graphs", get(graphs))
         .route("/graphs/graph", get(graphs_graph))
         .route("/graphs/tree", get(graphs_tree))
@@ -73,6 +76,15 @@ pub async fn router(
         .route("/metrics/num_schnicks", get(metrics_num_schnicks))
         .route("/metrics/streak", get(metrics_streak))
         .route("/metrics/num_invites", get(metrics_num_invites))
+        .route_layer(from_fn_with_state(state.clone(), redirect_if_in_schnick))
+        .route("/schnick", get(schnick))
+        .route("/home/sse", get(home_sse))
+        .route("/home/invite", get(home_invite))
+        .route("/schnick", post(schnick_submit))
+        .route("/schnick/sse", get(schnick_sse))
+        .route("/schnick/abort", get(schnick_abort))
+        .route("/settings/dect", post(settings_dect))
+        .route("/settings/username", post(settings_username))
         .route_layer(from_fn_with_state(state.clone(), Authenticator::layer))
         .with_state(state.clone());
     let router = Router::new()
