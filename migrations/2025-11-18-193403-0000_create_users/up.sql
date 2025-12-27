508b144883ee1e55fc79df69b8afccd965405f3b
Helena Jäger <mail@helena.place>
2025-12-27 19:21:26 +0100

    fix: removed uuid extension from migrations

--- migrations/2025-11-18-193403-0000_create_users/up.sql
+++ migrations/2025-11-18-193403-0000_create_users/up.sql
@@ -1,12 +1,11 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username varchar(32) NOT NULL UNIQUE,
     dect char(4),
     parent integer references users(id) NOT NULL,
-    token uuid NOT NULL DEFAULT uuid_generate_v4(),
+    token uuid NOT NULL DEFAULT uuidv4(),
     created timestamptz NOT NULL DEFAULT now(),
     active boolean NOT NULL DEFAULT false
 );
 
-INSERT INTO users (username, dect, parent, token, created, active) VALUES ('root', '5000', lastval(), uuid_generate_v4(), now(), true);
+INSERT INTO users (username, dect, parent, token, created, active) VALUES ('root', '5000', lastval(), uuidv4(), now(), true);
